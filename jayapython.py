b8e1ec23dda7605da7d68b070fbe846ae10d122b
jayakrishna <157739427+jayakrishna15@users.noreply.github.com>
2024-02-09 12:08:23 +0530

    If -else Statement

---
+++ jayapython.py
@@ -0,0 +1,16 @@
+a=52
+b=78
+c=a+b
+print(c)
+print("this is my first print")
+
+
+age = 19
+if age < 18:
+    print("you can't vote!")
+else:
+    print("you can vote!")
+    print("done!")
+
+    
+     
